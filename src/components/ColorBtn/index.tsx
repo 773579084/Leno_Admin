205bbde3b04fe4b65f61597a82165be83221d841
zhaowenchao <773579084@qq.com>
2023-03-08 11:31:51 +0800

    refactor:重构colorBtn组件

--- src/components/ColorBtn/index.tsx
+++ src/components/ColorBtn/index.tsx
@@ -1,90 +1,37 @@
-import React, { MouseEventHandler, ReactNode } from 'react'
+import React from 'react'
 import classes from './index.module.scss'
-import { Button } from 'antd'
+import { Button, ButtonProps } from 'antd'
+import classNames from 'classnames'
 
-const ColorBtn = (props: {
-  block: boolean
-  disabled: boolean
-  href: string
-  target: string
-  shape: 'default' | 'circle' | 'round'
-  size: 'large' | 'middle' | 'small'
-  icon: ReactNode
-  // btn 颜色种类
-  color: 'primary' | 'success' | 'info' | 'warning' | 'danger'
-  loading: boolean
-  // 插槽
-  children: string | null
-  onClick: MouseEventHandler<HTMLElement>
-}) => {
-  const { block, disabled, href, target, shape, size, icon, color, loading, onClick, children } =
-    props
+export interface ButtonPropType {
+  color?: 'primary' | 'success' | 'info' | 'warning' | 'danger'
+}
+const ColorBtn: React.FC<ButtonProps & ButtonPropType> = (props) => {
+  // prefixCls 完全重写覆盖ant button
+  const { disabled, children, color } = props
 
-  const styleClassFn = (): string => {
-    if (color === 'primary' && !disabled) {
-      return classes['primary-color-normal']
-    } else if (color === 'primary' && disabled) {
-      return classes['primary-color-disabled']
-    }
-
-    if (color === 'success' && !disabled) {
-      return classes['success-color-btn']
-    } else if (color === 'success' && disabled) {
-      return classes['success-color-disabled']
-    }
-
-    if (color === 'danger' && !disabled) {
-      return classes['danger-color-btn']
-    } else if (color === 'danger' && disabled) {
-      return classes['danger-color-disabled']
-    }
-
-    if (color === 'info' && !disabled) {
-      return classes['info-color-btn']
-    } else if (color === 'info' && disabled) {
-      return classes['info-color-disabled']
-    }
-
-    if (color === 'warning' && !disabled) {
-      return classes['warning-color-btn']
-    } else if (color === 'warning' && disabled) {
-      return classes['warning-color-disabled']
-    }
-
-    return classes['primary-color-normal']
-  }
+  const styleClass = classNames({
+    [classes['color-primary-normal']]: color === 'primary' && !disabled,
+    [classes['color-primary-disabled']]: color === 'primary' && disabled,
+    [classes['color-success-normal']]: color === 'success' && !disabled,
+    [classes['color-success-disabled']]: color === 'success' && disabled,
+    [classes['color-danger-normal']]: color === 'danger' && !disabled,
+    [classes['color-danger-disabled']]: color === 'danger' && disabled,
+    [classes['color-info-normal']]: color === 'info' && !disabled,
+    [classes['color-info-disabled']]: color === 'info' && disabled,
+    [classes['color-warning-normal']]: color === 'warning' && !disabled,
+    [classes['color-warning-disabled']]: color === 'warning' && disabled,
+  })
 
   return (
-    <span className={styleClassFn()}>
-      <Button
-        block={block}
-        disabled={disabled}
-        href={href}
-        target={target}
-        shape={shape}
-        size={size}
-        icon={icon}
-        loading={loading}
-        onClick={onClick}
-      >
-        {children}
-      </Button>
+    <span className={styleClass}>
+      <Button {...props}>{children}</Button>
     </span>
   )
 }
 
 ColorBtn.defaultProps = {
-  color: 'primary',
-  block: false,
-  disabled: false,
-  href: null,
-  target: null,
-  icon: null,
-  loading: false,
-  shape: 'default',
-  children: null,
-  size: 'middle',
-  onClick: () => {},
+  color: 'primary', // 'primary' | 'success' | 'info' | 'warning' | 'danger'
 }
 
 export default ColorBtn
